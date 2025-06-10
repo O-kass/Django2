003e8f8e63147457de175d20231240cb194d5f9e
Omar <kassom771@gmail.com>
2025-06-10 20:38:40 +0100

    modify views such that posts are seen

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 from django.template.context_processors import request
 
+posts = [
+    {
+        'author': 'OmarMK',
+        'title': "BlogPost_1",
+        'content': 'TEST1- If you can see this message the test blog is up and works',
+        'date': "09/06/25",
+    },
+    {
+        'author': 'OmarMK',
+        'title': "BlogPost_2",
+        'content': 'TEST2- If you can see this message multiple blogs can be seen at once',
+        'date': "09/06/25",
+    },
+
+]
+
+
 
 # Create your views here.
 def home(request):
-    return render(request, "blog/home.html")
+    context = {
+        'posts': posts
+    }
+    return render(request, "blog/home.html", context)
 
 def about(request):
-    return render(request, "blog/about.html")
+    return render(request, "blog/about.html", {"title": "ABOUT PAGE"})
